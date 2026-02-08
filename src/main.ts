7074a802292fe363bdaabde326f79477c60c5aed
juancodev <montillasanchezjuancarlos@gmail.com>
2026-02-08 19:51:38 -0400

    🐛 Fix problem and finish last details

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     }),
   );
 
-  //app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
