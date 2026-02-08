7e43e61f9e8764238e761950d71ffc9dd3c41176
juancodev <montillasanchezjuancarlos@gmail.com>
2026-02-08 15:27:13 -0400

    🚧 Creating dto of request-payment

---
+++ src/wallet/dto/request-payment.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsString, IsNumber, Min } from 'class-validator';
+
+export class RechargeWalletDto {
+  @IsNotEmpty({ message: 'El Documento es requerido' })
+  @IsString()
+  document: string;
+
+  @IsNotEmpty({ message: 'El teléfono celular es requerido' })
+  @IsString()
+  celular: string;
+
+  @IsNotEmpty({ message: 'El valor es requerido' })
+  @IsNumber({}, { message: 'El valor debe ser un número' })
+  @Min(1, { message: 'El valor debe ser mayor a 0' })
+  valor: number;
+}
